fb6f231f589eb6d66a12f9424ec7dae32bfd5246
rahul verma <rahulvermacode@gmail.com>
2025-01-05 22:03:14 +0530

    solved

--- star pattern/star pattern4.cpp
+++ star pattern/star pattern4.cpp
@@ -5,7 +5,7 @@ int main()
 {
     for(int i=1;i<=5;i++){
         for(int j=1;j<=5;j++){
-            if(j<=i){
+            if(j>=6-i){
                 cout<<"*";
             }
             else{
